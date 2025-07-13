2cf6cd14c258f1d06ab88ccf1fd82f7e167a2c77
parsamre <p.mre@yahoo.com>
2025-07-13 15:36:40 -0400

    initial commit

---
+++ src/test/java/com/parsamre/vocabmaster/VocabmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsamre.vocabmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabmasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
